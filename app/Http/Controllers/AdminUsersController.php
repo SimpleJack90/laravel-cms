4df86d393c034bd4f301a2ab7a4b6039261e10bc
SimpleJack90 <nemanja90petrovic@hotmail.com>
2019-01-03 14:41:44 +0100

    users section working 100% - security

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Collection;
 use App\User;
 use App\Role;
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UsersEditRequest;
 
 class AdminUsersController extends Controller
 {
@@ -87,6 +88,11 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
+        $user=User::findOrFail($id);
+
+        $roles=Role::pluck('name','id')->all();
+
+        return view('admin.users.edit',compact('user','roles'));
     }
 
     /**
@@ -96,9 +102,33 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
         //
+        $user = User::findOrFail($id);
+
+        if(trim($request->password)==''){
+            $input=$request->except('password');
+        }else{
+            $input=$request->all();
+            $input['password']=bcrypt($request->password);
+        }
+
+
+
+        if($file=$request->file('photo_id')){
+            $name=time().$file->getClientOriginalName();
+            $file->move('images',$name);
+            $photo=Photo::create(['file'=>$name]);
+
+            $input['photo_id']=$photo->id;
+
+        }
+
+
+        $user->update($input);
+
+        return redirect('/admin/users');
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -4,13 +4,21 @@
 
 
 @section('content')
-
+<style>
+    #editUser:hover{
+        color:blue;
+        text-decoration: underline;
+    }
+</style>
 <h1>Users</h1>
 
-    <table class="table  table-responsive-sm table-hover table-striped">
+
+
+    <table class="table  table-responsive-sm  ">
         <thead>
         <tr>
             <th>Id:</th>
+            <th>Photo:</th>
             <th>Name:</th>
             <th>Email:</th>
             <th>Role:</th>
@@ -25,7 +33,8 @@
         @foreach($users as $user)
         <tr>
             <td>{{$user->id}}</td>
-            <td>{{$user->name}}</td>
+            <td><img height="50" src="{{$user->photo?URL::asset($user->photo->file):'https://via.placeholder.com/200x100'}}"></td>
+            <td><a id="editUser" href="{{route('users.edit',$user->id)}}">{{$user->name}}</a></td>
             <td>{{$user->email}}</td>
             <td>{{$user->role->name}}</td>
             <td> {{$user->is_active==1 ? 'Active':'Not Active'}} </td>
